ab3b490e1a9dbd3a9257edbd25c6702cee1f3c0d
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-07-07 13:24:28 -0400

    Summary Algorithm

---
+++ summarization.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+from transformers import pipeline
+
+from datasets import load_dataset
+
+xsum_dataset = load_dataset("xsum")
+xsum_sample = xsum_dataset["train"].select(range(10))
+print(xsum_sample.to_pandas())
+summarizer = pipeline(task="summarization", model="sshleifer/distilbart-cnn-12-6", max_length=60, truncation=True)
+
+# Apply the pipeline to the batch of articles in `xsum_sample`
+summarization_results = summarizer(xsum_sample["document"])
+summarization_results
+
+
+import pandas as pd
+
+print(
+    pd.DataFrame.from_dict(summarization_results).rename({"summary_text":"generated_summary"},axis=1).join(
+        pd.DataFrame.from_dict(xsum_sample))[["generated_summary", "summary", "document"]])
