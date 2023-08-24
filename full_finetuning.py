e2d0c35b35beb41fd2e9995458a030a49f8f087f
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-08-24 07:42:58 -0400

    Finetuning & PEFT

--- full_finetuning.py
+++ full_finetuning.py
@@ -104,3 +104,54 @@ trainer = Trainer(
 )
 
 trainer.train()
+
+
+# we will use this to evaluate our model and see how good is the model
+
+dialogues = dataset['test'][0:10]['dialogue']
+human_baseline_summaries = dataset['test'][0:10]['summary']
+
+original_model_summaries = []
+instruct_model_summaries = []
+
+for _, dialogue in enumerate(dialogues):
+    prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary: """
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+    original_model_summaries.append(original_model_text_output)
+
+    instruct_model_outputs = instruct_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
+    instruct_model_summaries.append(instruct_model_text_output)
+    
+zipped_summaries = list(zip(human_baseline_summaries, original_model_summaries, instruct_model_summaries))
+ 
+df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'original_model_summaries', 'instruct_model_summaries'])
+df
+
+
+original_model_results = rouge.compute(
+    predictions=original_model_summaries,
+    references=human_baseline_summaries[0:len(original_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+instruct_model_results = rouge.compute(
+    predictions=instruct_model_summaries,
+    references=human_baseline_summaries[0:len(instruct_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('ORIGINAL MODEL:')
+print(original_model_results)
+print('INSTRUCT MODEL:')
+print(instruct_model_results)
